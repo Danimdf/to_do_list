a6429cb38e4f97898fa3d9832f075b4e9e8ca06d
Daniela Monteiro <danimdf360@gmail.com>
2021-10-02 20:51:49 -0300

    parsing data with comma and in lines

--- lib/to_do_list.ex
+++ lib/to_do_list.ex
@@ -1,15 +1,36 @@
 defmodule ToDoList do
-  #read a file .txt with the content
+  #name of file
   def start do
     file = IO.gets("Name of .txt to load: ") |> String.trim
     read(file)
   end
 
+#read a file .csv with the content
   def read(file) do
     case File.read(file) do
       {:ok, body}       -> body
       {:error, reason} -> IO.puts ~s(file invalid "#{file}"\n)
-                          IO.inspect reason
+                          IO.puts ~s("#{:file.format_error reason}"\n)
+                          start()
     end
   end
+
+  #parse data with comma
+  def parse(body) do
+    [header | lines] = String.split(body, ~r{(\r\n|\r|\n)})
+    titles = tl String.split(header, ",")
+    parse_lines(lines, titles)
+  end
+
+  #parse data in lines
+  def parse_lines(lines, titles) do
+    Enum.reduce(lines, %{}, fn line, built ->
+      [name | fields] = String.split(line, ",")
+      if Enum.count(fields) == Enum.count(titles) do
+        line_data = Enum.zip(titles, fields) |> Enum.into(%{})
+        Map.merge(built, %{name => line_data})
+      else
+        built
+      end
+    end)
 end
