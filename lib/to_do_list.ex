848ad02aa43cebbb2c4961fc4aa6d1035e387d0c
Daniela Monteiro <danimdf360@gmail.com>
2021-10-02 21:56:22 -0300

    A prompt messages and options for the ToDo!

--- lib/to_do_list.ex
+++ lib/to_do_list.ex
@@ -3,6 +3,39 @@ defmodule ToDoList do
   def start do
     file = IO.gets("Name of .txt to load: ") |> String.trim
     read(file)
+    |> parse
+    |> get_command
+  end
+
+  def get_command(data) do
+    prompt = """
+Type the first letter of the command you want to run
+R)ead Todos    A)dd a Todo    D)elete a Todo    L)oad a .csv    S)ave a .csv
+"""
+    command = IO.gets(prompt)
+      |> String.trim
+      |> String.downcase
+
+    case command do
+      "r" -> show_todos(data)
+      "d" -> delete_todo(data)
+      "q" -> "Goodbye!"
+      _   -> get_command(data)
+    end
+  end
+
+  def delete_todo(data) do
+    todo = IO.gets("Which todo would you like to delete?\n") |> String.trim
+    if Map.has_key? data, todo do
+      IO.puts "ok."
+      new_map = Map.drop(data, [todo])
+      IO.puts ~s("#{todo}" has been deleted.)
+      get_command(new_map)
+    else
+      IO.puts ~s("There is no Todo named "#{todo}"!)
+      show_todos(data, false)
+      delete_todo(data)
+    end
   end
 
 #read a file .csv with the content
@@ -33,4 +66,15 @@ defmodule ToDoList do
         built
       end
     end)
+  end
+
+  def show_todos(data, next_command? \\ true) do
+    items = Map.keys data
+    IO.puts "You have the following Todos:\n"
+    Enum.each items, fn item -> IO.puts item end
+    IO.puts "\n"
+    if next_command? do
+      get_command(data)
+    end
+  end
 end
